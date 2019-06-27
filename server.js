89d4f29b953df00ccedb15be6482db4c50138951
semyers189 <semyers@fullsail.edu>
2019-06-27 00:20:00 -0400

    removed welcome message from api

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to the Contact Keeper api' });
-});
+// app.get('/', (req, res) => {
+//   res.json({ msg: 'Welcome to the Contact Keeper api' });
+// });
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
